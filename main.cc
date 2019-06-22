51645a8dc801fd212beb64147b91a5d9fd117950
Nikhil Shende <nikhilshende@Nikhils-MacBook-Pro.local>
2019-06-21 21:41:32 -0400

    Random Word Selection

--- main.cc
+++ main.cc
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <ctime>
+#include <cstdlib>
 
-std::ifstream wordfile("words.txt");
+const int textfileLength = 80045;
 
 void playGame(std::string word){
 
@@ -11,8 +13,15 @@ void playGame(std::string word){
 
 int main(){
     bool playAgain = true;
+    srand(time(0));
     while(playAgain){
-
+        int r = ((double) rand() / (RAND_MAX)) * textfileLength + 1;
+        std::string s;
+        std::ifstream wordfile("words.txt");
+        for(int i = 0; i < r; ++i){
+            getline(wordfile,s);
+        }
+        playGame(s);
         while(1){
             std::cout << "Would you like to play again? 'Y' or 'N'" << std::endl;
             char c;
